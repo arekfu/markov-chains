11e107668bb518cebab88de30d5ba11685a369bc
Davide Mancusi <davide.mancusi@cea.fr>
2015-09-29 18:35:43 +0200

    Simulate n Markov chains

--- markov-chains.hs
+++ markov-chains.hs
@@ -11,24 +11,30 @@ import MarkovChain
 
 
 
-process :: Int -> Double -> Seed -> IO ()
-process size absorptionProbability seed = do
+process :: Int -> Double -> Int -> Seed -> IO ()
+process size absorptionProbability shots seed = do
     let gen = mkStdGen seed
     let (m, gen') = runMC (mkTransitionMatrix size absorptionProbability) gen
     putStrLn "Generated transition matrix:"
     print m
-    let (steps, gen'') = runMC (runMarkovChain stepUntilAbsorption m 1) gen'
-    putStrLn "Generated Markov chain:"
-    print $ fst steps
+    let (steps, gen'') = runMC (simulateNChains shots 1 m) gen'
+    putStrLn "Generated Markov chains:"
+    print steps
 
+simulateNChains :: Int -> SystemState -> TransitionMatrix -> MC [[SystemState]]
+simulateNChains shots initialState matrix = do
+    chains <- replicateM shots $ runMarkovChain stepUntilAbsorption matrix initialState
+    return $ map fst chains
 
 defaultSize = 15
 defaultAbsorptionProbability = 0.1
 defaultSeed = 12345
+defaultShots = 10
 
 main = do
     args <- getArgs
     let size = if length args > 0 then read $ head args else defaultSize
     let absorptionProbability = if length args > 1 then read $ args !! 1 else defaultAbsorptionProbability
-    let seed = if length args > 2 then read $ args !! 2 else defaultSeed
-    process size absorptionProbability seed
+    let shots = if length args > 2 then read $ args !! 2 else defaultShots
+    let seed = if length args > 3 then read $ args !! 3 else defaultSeed
+    process size absorptionProbability shots seed
